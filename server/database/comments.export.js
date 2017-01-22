4cdc9b5d1b276c05f38535e63a550f11a3999c4a
Ilya Litvinov <ilyaillych@gmail.com>
2017-01-22 23:25:24 +0200

    Merge branch 'master' into 29-10-16

--- server/database/comments.export.js
+++ server/database/comments.export.js
@@ -18,31 +18,23 @@ mongoose.connect(`mongodb://localhost/${dbName}`);
 mongoose.set('debug', true);
 
 function CreateCollection() {
-    return PhoneModel.find({name: 'Dell Streak 7'})
-        .then(phone => {
-            const input = {
-                item_id: phone[0].id,
-                text: 'comment for phone',
-                author: 'test author',
-                created_on: new Date().getTime(),
-                item_rating: 4
-            };
+    return PhoneModel.find({})
+        .then(phones => {
+            const pomises = phones.map((phone) => {
+                const input = {
+                    item_id: phone.id,
+                    text: `Default comment for phone ${phone.name}`,
+                    author: 'Default Author',
+                    created_on: Date.now(),
+                    item_rating: Math.round(Math.random()*5)
+                };
 
-            return new CommentModel(input).save()
+                return new CommentModel(input).save()
+            });
+
+            return Promise.all(pomises)
         })
         .then(res => {
-            const input = {
-                item_id: res.id,
-                text: 'comment for comment',
-                author: 'test author',
-                created_on: new Date().getTime(),
-                item_rating: 4
-            };
-
-            return new CommentModel(input).save()
-        })
-        .then(() => {
-            console.log('>>>>>>>>>>>>>>>>>>>>>');
             process.exit();
         })
         .catch(e => {
@@ -54,9 +46,9 @@ function CreateCollection() {
 }
 mongoose.connection.on('open', () => {
     mongoose.connection.db.dropCollection('comments', (err, result) => {
-        mongoose.connection.db.collections().then(() => {
+        mongoose.connection.db.collections().then(res => {
             //todo create auto droping db if exist
-            return CreateCollection();
+            CreateCollection()
         })
     });
 });
